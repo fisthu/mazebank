44276a09b91fd5d024ff8b9affd3a2b7258cf594
Oki Fisthu Sasongko <okfisthu@gmail.com>
2023-01-19 06:26:31 +0700

    login ui

--- src/main/java/com/fisthu/mazebank/App.java
+++ src/main/java/com/fisthu/mazebank/App.java
@@ -1,13 +1,18 @@
 package com.fisthu.mazebank;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/fisthu/mazebank/controller/LoginController.java
+++ src/main/java/com/fisthu/mazebank/controller/LoginController.java
@@ -1,4 +1,16 @@
 package com.fisthu.mazebank.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
 
+    public ChoiceBox accountSelector;
+    public Label payeeAddressLbl;
+    public TextField payeeAddressField;
+    public TextField passwordField;
+    public Button loginBtn;
+    public Label errorLabel;
 }
