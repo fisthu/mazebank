d5926a904035a33f1d40bfce27dd29680f2798f5
Oki Fisthu Sasongko <okfisthu@gmail.com>
2023-01-19 13:05:24 +0700

    client menu ui

---
+++ src/main/java/com/fisthu/mazebank/controller/client/ClientMenuController.java
@@ -0,0 +1,21 @@
+package com.fisthu.mazebank.controller.client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboardBtn;
+    public Button transactionBtn;
+    public Button accountBtn;
+    public Button profileBtn;
+    public Button logoutBtn;
+    public Button reportBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
