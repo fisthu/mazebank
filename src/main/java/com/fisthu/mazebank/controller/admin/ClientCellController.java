565d96fa42cd3ca9fb7acabeee524e3bfa681e85
Oki Fisthu Sasongko <okfisthu@gmail.com>
2023-01-23 05:34:55 +0700

    client cell UI

---
+++ src/main/java/com/fisthu/mazebank/controller/admin/ClientCellController.java
@@ -0,0 +1,23 @@
+package com.fisthu.mazebank.controller.admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCellController implements Initializable {
+    public Label firstnameLbl;
+    public Label lastnameLbl;
+    public Label pAddressLbl;
+    public Label chAccLbl;
+    public Label svAccLbl;
+    public Label dateLbl;
+    public Button deleteBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
