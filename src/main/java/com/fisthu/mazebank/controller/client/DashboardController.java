4b89996957e4980553e62fadecc063fb699e11c6
Oki Fisthu Sasongko <okfisthu@gmail.com>
2023-01-24 23:32:47 +0700

    client dashboard

--- src/main/java/com/fisthu/mazebank/controller/client/DashboardController.java
+++ src/main/java/com/fisthu/mazebank/controller/client/DashboardController.java
@@ -1,10 +1,15 @@
 package com.fisthu.mazebank.controller.client;
 
+import com.fisthu.mazebank.model.Client;
+import com.fisthu.mazebank.model.Model;
+import com.fisthu.mazebank.model.Transaction;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
@@ -16,7 +21,7 @@ public class DashboardController implements Initializable {
     public Label savingAccNoLbl;
     public Label incomeLbl;
     public Label expenseLbl;
-    public ListView transListView;
+    public ListView<Transaction> transListView;
     public TextField payeeAddressField;
     public TextField amountField;
     public TextArea messageTxtArea;
@@ -24,6 +29,16 @@ public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        bind();
+    }
 
+    private void bind() {
+        Client client = Model.INSTANCE.getClient();
+        usernameTxt.textProperty().bind(Bindings.concat("Hi, ").concat(client.firstnameProperty()));
+        loginDateLbl.setText("Today, %s".formatted(LocalDate.now()));
+        checkingBalanceLbl.textProperty().bind(client.checkingAccountProperty().get().balanceProperty().asString());
+        checkingAccountNumberLbl.textProperty().bind(client.checkingAccountProperty().get().accountNumberProperty());
+        savingBalanceLbl.textProperty().bind(client.savingAccountProperty().get().balanceProperty().asString());
+        savingAccNoLbl.textProperty().bind(client.savingAccountProperty().get().accountNumberProperty());
     }
 }
