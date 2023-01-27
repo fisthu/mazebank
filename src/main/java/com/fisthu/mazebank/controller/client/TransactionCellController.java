8f7f0b59230ddd47dcb0c84b49d35c1ccdef2e50
Oki Fisthu Sasongko <okfisthu@gmail.com>
2023-01-27 11:32:23 +0700

    handle transaction icon color

--- src/main/java/com/fisthu/mazebank/controller/client/TransactionCellController.java
+++ src/main/java/com/fisthu/mazebank/controller/client/TransactionCellController.java
@@ -1,32 +1,49 @@
 package com.fisthu.mazebank.controller.client;
 
+import com.fisthu.mazebank.model.Model;
 import com.fisthu.mazebank.model.Transaction;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TransactionCellController implements Initializable {
-    public Label transDateLbl;
-    public FontAwesomeIconView inIcon;
-    public FontAwesomeIconView outIcon;
-    public Label senderLbl;
-    public Label receiverLbl;
-    public Label amountLbl;
+  public Label transDateLbl;
+  public FontAwesomeIconView inIcon;
+  public FontAwesomeIconView outIcon;
+  public Label senderLbl;
+  public Label receiverLbl;
+  public Label amountLbl;
 
-    private final Transaction transaction;
+  private final Transaction transaction;
 
-    public TransactionCellController(Transaction transaction) {
-        this.transaction = transaction;
+  public TransactionCellController(Transaction transaction) {
+    this.transaction = transaction;
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    senderLbl.textProperty().bind(transaction.senderProperty());
+    receiverLbl.textProperty().bind(transaction.receiverProperty());
+    amountLbl.textProperty().bind(transaction.amountProperty().asString());
+    transDateLbl.textProperty().bind(transaction.dateProperty().asString());
+
+    transactionIcon();
+  }
+
+  private void transactionIcon() {
+    if (transaction
+        .senderProperty()
+        .get()
+        .equalsIgnoreCase(Model.INSTANCE.getClient().payeeAddressProperty().get())) {
+      inIcon.setFill(Color.rgb(240, 240, 240));
+      outIcon.setFill(Color.RED);
+    } else {
+      inIcon.setFill(Color.GREEN);
+      outIcon.setFill(Color.rgb(240, 240, 240));
     }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        senderLbl.textProperty().bind(transaction.senderProperty());
-        receiverLbl.textProperty().bind(transaction.receiverProperty());
-        amountLbl.textProperty().bind(transaction.amountProperty().asString());
-        transDateLbl.textProperty().bind(transaction.dateProperty().asString());
-    }
+  }
 }
