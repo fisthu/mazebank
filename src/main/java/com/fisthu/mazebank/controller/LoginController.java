4d671981ba8c143e0c877e77293b615d8b88b6d5
Oki Fisthu Sasongko <okfisthu@gmail.com>
2024-09-18 12:27:07 +0700

    change address label1

--- src/main/java/com/fisthu/mazebank/controller/LoginController.java
+++ src/main/java/com/fisthu/mazebank/controller/LoginController.java
@@ -40,7 +40,7 @@ public class LoginController implements Initializable {
     private void listenOnSelectorChange() {
         Model.INSTANCE.getViewFactory().setLoginAccountType(accountSelector.getValue());
         if (Model.INSTANCE.getViewFactory().getLoginAccountType() == AccountType.CLIENT) {
-            payeeAddressLbl.setText("Payee Address:");
+            payeeAddressLbl.setText("Payee Address::");
         } else {
             payeeAddressLbl.setText("Username:");
         }
